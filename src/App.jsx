c3798b3f8615530ff72b21372256666749ce02fe
Jay Lloyd Daquipa <31461040+JLDaquipa@users.noreply.github.com>
2023-11-28 21:52:42 +0800

    Added setup files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <h1 className="text-4xl font-clashDisplay font-semibold">
+      Discover more than 
+      <span className="text-blue-500 block"> 5000+ Jobs</span>
+    </h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        epilogue: ['Epilogue', 'sans-serif'],
+        clashDisplay: ['Clash Display', 'sans-serif'],
+      },
+      colors: {
+        'primary': "#FFFFFF",
+        "primary-blue": "#4640DE",
+        "accent-blue": "#26A4FF",
+        "accent-green": "#56CDAD",
+        "accent-red": "#FF6550",
+        "accent-yellow": "#FFB836",
+        "slate-gray": "#D6DDEB",
+      },
+      boxShadow: {
+        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+      backgroundImage: {
+        'hero': "url('assets/images/hero-background.svg')",
+      },
+      screens: {
+        "wide": "1440px"
+      }
+    },
+  },
+  plugins: [],
+}
